560d62502c396016ccef6918f1de951a8bcbe669
Ndg19 <158159787+Ndg19@users.noreply.github.com>
2025-10-07 12:02:16 +0300

    cors miiddilware added

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // Parse JSON request bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
+// CORS: allow frontend from env variable + local dev
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173"], // local dev fallback
+    credentials: true,
+  })
+);
+
 //  Test DB connection once before starting server
 (async () => {
   try {
